f3176b4eb45398c08258363ff7b5eca6138af41d
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-08-17 11:10:56 -0500

    ordenado e indentado

--- pruebac.cpp
+++ pruebac.cpp
@@ -1,10 +1,11 @@
 #include <iostream.h>
 using namespace 
 int main()
-{int a, b, c, suma;
-a=5;
-b=6;
-c=25;
-suma = a+ b + c;
-cout << suma << endl;
-return 0;}
+{
+	int a, b, c, suma;
+		a=5;
+		b=6;
+		c=25;
+		suma = a+ b + c;
+		cout << suma << endl;
+		return 0;}
