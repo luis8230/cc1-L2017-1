cb8507a5006fa60d0a6e77352d7c9470ed67bf5c
ALUMNO-D <ALUMNO-D@UCSP.AULAB>
2017-09-05 09:32:11 -0500

    decremento prueba

---
+++ Decremento/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main()
+{
+   int N;
+   cin >>N;
+   while(N-->0)
+    cout << "Hello world!" << endl;
+    return 0;
+}
+ */
+int main()
+{
+    int n;
+    cin>>n;
+    do{
+    cout << "Hello world!" << endl;
+     }
+     while(--n>0);
+
+    return 0;
+}
