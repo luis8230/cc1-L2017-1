7c2a5b41c4f0136c0be06daf9b40e9f0cb329b9d
luis8230 <31104963+luis8230@users.noreply.github.com>
2017-10-16 23:23:19 -0400

    Equipo CPP
    Faltan pruebas sobre su funcionamiento.

--- Equipo.cpp
+++ Equipo.cpp
@@ -1,16 +1,50 @@
 #include "Equipo.h"
-#include "jugador.h"
+#include "Jugador.h"
+#include <string>
+#include <iostream>
+
 
 equipo::equipo()
 {
     cout<<"debiste agregar jugadores para este equipo"<<endl;
 }
-equipo::equipo(string name,int players){
-    nombre=name;
-    jugadores = players;
-    for (int i=0; i<players; i++){
-        cin<<jugador;
-        cout<<nom<<camiseta<<lugar<<agress<<endl;
-    }
+
+equipo::equipo(string name,jugador ingreso[11]){
+    IngresoNombre(name);
+    Players(ingreso);
+}
+
+
+void equipo::IngresoNombre(string name){
+    nombre= name;
+}
+
+
+void equipo::Players(jugador ing[11]){
+    string Identificacion, Papel;
+    int Camiseta,indisciplina;
+    for(int i=0;i<11;i++){
+
+        cout<<"ingrese el nombre del jugador"<<endl;
+        cin>>Identificacion;
+
+        cout<<"ingrese el numero de la camiseta del jugador"<<endl;
+        cin>>Camiseta;
+
+        cout<<"Ingrese la posicion del jugador"<<endl;
+        cin>>Papel;
+
+        cout<<"ingrese que tan agresivo se encuentra el jugador"<<endl;
+        cin>>indisciplina;
+
+        ing[i];
+        }
+}
+
+
+
+
+void equipo::Impresion(){
+    cout<<"El nombre del equipo es "<<nombre<<endl;
 }
 
