a846e7ab781d68bf6fe9e7d1efe209d2622dff9a
fsbenavides <31104984+fsbenavides@users.noreply.github.com>
2017-11-21 09:50:30 -0500

    Add files via upload

--- escenario.cpp
+++ escenario.cpp
@@ -1,7 +1,20 @@
 #include "escenario.h"
 
-escenario::escenario(string )
+escenario::escenario(string a, BITMAP b,string c,int x, int y )
 {
-    //ctor
+   cargar(a,b,c);
+   pintar(x,y);
 }
 
+void escenario::cargar(string a, BITMAP b,string c)
+{
+    buffer=b;
+    esc=load_bmp(a,NULL);
+    colisiones=load_bmp(c,NULL);
+
+}
+
+void escenario::pintar(int x,int y)
+{
+    blit(esc,buffer,0,0,x,y,640,400);
+}
